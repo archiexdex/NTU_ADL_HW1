bfa3df3ff15eb0122e56ae9007b401e1443ec3ff
XDEX <rc840327@gmail.com>
2020-04-07 23:39:10 +0800

    fix download bug

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-wget https://www.dropbox.com/s/f6dctchfk4e2ewd/datasets.zip?dl=1
-unzip datasets
+wget https://www.dropbox.com/s/f6dctchfk4e2ewd/datasets.zip?dl=1 -O datasets.zip
+unzip datasets.zip
